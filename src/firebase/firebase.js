3b67989f41ddb7d4aa5f2257da6f9c08a4efdd9e
Aditya Jadhav <39948420+Adityaaj18@users.noreply.github.com>
2021-04-21 12:39:59 +0530

    another commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,7 +4,7 @@ import '@firebase/auth';
 
 const config = {
   apiKey: "AIzaSyCgqJ32reST0kgVTTfIpCMmm0u8SzuJXew",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  authDomain: "expensify-944cc.firebaseapp.com",
   databaseURL: "https://expensify-944cc-default-rtdb.firebaseio.com",
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
